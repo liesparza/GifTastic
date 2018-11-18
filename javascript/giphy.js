b00bceaf0244573d3481bde394c30754cc343cab
liesparza <lir.marcelae@hotmail.com>
2018-11-17 22:51:08 -0600

    Add files via upload

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,12 +1,8 @@
 
 /* TO DO 
 
-- Call the Api
-- Creating from 6-7 variables with differents buttons to be created that call a different topic
-- Function to create a button adding and to search for that topic
-- Display all the gif (limited to 10) in a specif div
-- Display all the rating a another column
-- Stop and Animate gif from a clic 
+- Function to create a button adding and to search for that topic ( doesn't define the other's value)
+- Stop and Animate gif from a click 
 - If a button clicked and if there are gif displayed then it will clear and only show 
 what are called 
 
@@ -16,7 +12,7 @@ var cartoons = ["Hey Arnold", "Daria", "Spongebob"];
 
  
 
-// It ONLY CALLS REAL MONSTERS DOESN'T LET THE OTHER DATAS BE SEARCH
+// It ONLY CALLS the first one DOESN'T LET THE OTHER DATAS BE SEARCH
 //$("button").on("click",
    function displayGiphy() {
   
@@ -33,7 +29,6 @@ var cartoons = ["Hey Arnold", "Daria", "Spongebob"];
         var results= response.data;
 
 
-    //  $("#gif").text(JSON.stringify(response));
 
       for (var i = 0; i < results.length; i++) {
 
@@ -50,7 +45,7 @@ var cartoons = ["Hey Arnold", "Daria", "Spongebob"];
         
         giphyDiv.append(giphyImage);
         
-        $("#gif").append(giphyDiv); // before preppend
+        $("#gifs-view").append(giphyDiv); // before preppend
          }
         
 
@@ -64,36 +59,27 @@ var cartoons = ["Hey Arnold", "Daria", "Spongebob"];
  function renderButtons() {
 
     // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
     $("#giphy-buttons").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < cartoons.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array.
-      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
       var a = $("<button>");
       // Adding a class
       a.addClass("gif");
-      // Adding a data-attribute with a value of the movie at index i
       a.attr("data-name", cartoons[i]);
-      // Providing the button's text with a value of the movie at index i
       a.text(cartoons[i]);
       // Adding the button to the HTML
       $("#giphy-buttons").append(a);
     }
  }
 
-      // This function handles events where a movie button is clicked
       $("#add-giphy").on("click", function(event) {
         event.preventDefault();
-        // This line grabs the input from the textbox
         var gifs = $("#giphy-input").val().trim();
 
-        // Adding movie from the textbox to our array
         cartoons.push(gifs);
 
-        // Calling renderButtons which handles the processing of our movie array
         renderButtons();
       });
 
